ba707ae5a580165f99178fba98236347df49ec7e
podrazaa <podrazaa@edb23e68-2225-11de-9288-5f72e443383c>
2009-04-05 23:15:21 +0000

    Added ITD interaface, and working version of TDMC

---
+++ trunk/TD/src/GameData.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/*
+ * Information about game played.
+ */
+public class GameData {
+	
+	/*
+	 * Holds evaluation function features values for every 
+	 * time moment t = 1,2.....T. 
+	 */
+	List<double[]> evaluationFunctionFeatures;
+	
+	//need to add some field that would provide information about situation on the board
+	//in every time moment: 1,2,....,T
+}
