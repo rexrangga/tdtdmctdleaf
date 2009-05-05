0e966a0805c9e2e5a0d257864066217872586d1d
podrazaa <podrazaa@edb23e68-2225-11de-9288-5f72e443383c>
2009-05-05 19:39:21 +0000

    Updated evaluation function calculations.

--- TD/src/draughts/TDMC.java
+++ TD/src/draughts/TDMC.java
@@ -1,143 +1,155 @@
 package draughts;
+
 import java.util.List;
 
 /*
  * Implementation of the  TDMC (as described in the Osaki, Shibahara Tajima and Kotani article)
  */
-public class TDMC extends TD 
-{
-	double gammaDiscountRate=0.5;
-	double lambdaEligibilityRate=0.5;
+public class TDMC extends TD {
+	double gammaDiscountRate = 0.5;
+	double lambdaEligibilityRate = 0.5;
 	double alfaLearningRate = 0.2;
-	
-	public TDMC(double[] weights, 
-				double gammaDiscountRate, 
-				double lambdaEligibilityRate,
-				double alfaLearningRate) {
-		super(weights);
+
+	public TDMC(double[] weights, double gammaDiscountRate,
+			double lambdaEligibilityRate, double alfaLearningRate, double a,
+			double b) {
+		super(weights, a, b);
 		this.gammaDiscountRate = gammaDiscountRate;
 		this.lambdaEligibilityRate = lambdaEligibilityRate;
 		this.alfaLearningRate = alfaLearningRate;
 	}
 
-	@Override
 	public void updateWeights(GameData gameData) {
-		//first step of the algorithm - calculate evaluationFunctionValues
+		// first step of the algorithm - calculate evaluationFunctionValues
 		double[] evaluationFunctionValues = calculateEverEvaluationFunctionValue(gameData.evaluationFunctionFeatures);
-		//calculate evaluation function gradient
-		//first dimension - time moment, second dimension - weight derrivates
+		// calculate evaluation function gradient
+		// first dimension - time moment, second dimension - weight derrivates
 		double[][] evaulationFunctionGradient = computeEvaluationFunctionGradient(gameData.evaluationFunctionFeatures);
 		double[] rewards = calculateWinningProbabilities(gameData);
 		double[] Rt = computeReturn(rewards);
-		double[][] RTNStep = computeNStepReturn(rewards, evaluationFunctionValues);
+		double[][] RTNStep = computeNStepReturn(rewards,
+				evaluationFunctionValues);
 		double[] RTLambda = computeLabdaReturn(RTNStep, Rt);
-		
-		updateWeights(RTLambda, evaulationFunctionGradient, evaluationFunctionValues);
+
+		updateWeights(RTLambda, evaulationFunctionGradient,
+				evaluationFunctionValues);
 	}
-	
-	private double[] calculateWinningProbabilities(GameData gameData){
-		//TODO napisaï¿½ kod symulacji MonteCarlo
-		
+
+	private double[] calculateWinningProbabilities(GameData gameData) {
+		// TODO napisaæ kod symulacji MonteCarlo
+
 		return null;
 	}
-	
+
 	/**
 	 * Calculates evaluation function values for every time moment
 	 */
-	private double[] calculateEverEvaluationFunctionValue(List<double[]> features)
-	{
+	private double[] calculateEverEvaluationFunctionValue(
+			List<double[]> features) {
 		double[] functionValues = new double[features.size()];
-		for(int i=0; i<features.size(); i++)
-		{
-			functionValues[i]= calculateEvaluationFunction(features.get(i));
+		for (int i = 0; i < features.size(); i++) {
+			functionValues[i] = calculateEvaluationFunction(features.get(i));
 		}
 		return functionValues;
 	}
-	
+
 	/**
 	 * Calculates gradient of the evaluation function for every moment of time.
-	 * Return value: first dimension - time moment, second dimension - weight derrivate
+	 * Return value: first dimension - time moment, second dimension - weight
+	 * derrivate
 	 */
-	private double[][] computeEvaluationFunctionGradient(List<double[]> evaluationFunctionFeatures)
-	{
+	private double[][] computeEvaluationFunctionGradient(
+			List<double[]> evaluationFunctionFeatures) {
 		double[][] gradient = new double[evaluationFunctionFeatures.size()][weights.length];
-		for(int i=0; i<evaluationFunctionFeatures.size(); i++){
-			for(int j=0; j<weights.length; j++){
-				gradient[i][j]=evaluationFunctionFeatures.get(i)[j];
+		for (int i = 0; i < evaluationFunctionFeatures.size(); i++) {
+			for (int j = 0; j < weights.length; j++) {
+				gradient[i][j] = evaluationFunctionFeatures.get(i)[j];
 			}
 		}
 		return gradient;
 	}
-	
+
 	/**
 	 * 
-	 * @param returns returns by Monte Carlo simulation for every time moment
+	 * @param returns
+	 *            returns by Monte Carlo simulation for every time moment
 	 * @return Rt return for every time moment
 	 */
-	private double[] computeReturn(double[] returns){
+	private double[] computeReturn(double[] returns) {
 		double[] Rt = new double[returns.length];
-		for(int i=0; i<returns.length; i++){
+		for (int i = 0; i < returns.length; i++) {
 			double value = 0.0;
-			for(int j=i; j<returns.length; j++)
-				value+=Math.pow(lambdaEligibilityRate, j-1)*returns[j];
-			Rt[i]=value;		
+			for (int j = i; j < returns.length; j++)
+				value += Math.pow(lambdaEligibilityRate, j - 1) * returns[j];
+			Rt[i] = value;
 		}
 		return Rt;
 	}
-	
+
 	/**
 	 * Calculates the n-step return for each moment of time
-	 * @return first argument - time moment, n-step return (from t to T-1, size od second dimension is (T-1-t))
+	 * 
+	 * @return first argument - time moment, n-step return (from t to T-1, size
+	 *         od second dimension is (T-1-t))
 	 */
-	private double[][] computeNStepReturn(double[] rewards, double[] evaluationFunctionValues){
+	private double[][] computeNStepReturn(double[] rewards,
+			double[] evaluationFunctionValues) {
 		double[][] Rn = new double[rewards.length][];
-		//foreach time moment
-		for(int i=0; i<rewards.length; i++){
-			//for t, t+1, t+2 .... t+n - build and fill table for different values of n = (t;T)
-			Rn[i]=new double[rewards.length-i];
-			for(int j=i; j<Rn[i].length+i; j++){
+		// foreach time moment
+		for (int i = 0; i < rewards.length; i++) {
+			// for t, t+1, t+2 .... t+n - build and fill table for different
+			// values of n = (t;T)
+			Rn[i] = new double[rewards.length - i];
+			for (int j = i; j < Rn[i].length + i; j++) {
 				double value = 0.0;
-				//for given value of n calculate sum
-				for(int k=i; k<j ;k++){
-					value+=Math.pow(lambdaEligibilityRate, k-i)*rewards[k];
+				// for given value of n calculate sum
+				for (int k = i; k < j; k++) {
+					value += Math.pow(lambdaEligibilityRate, k - i)
+							* rewards[k];
 				}
-				value+=Math.pow(lambdaEligibilityRate, (j-i))*evaluationFunctionValues[j];
-				Rn[i][j-i]=value;
+				value += Math.pow(lambdaEligibilityRate, (j - i))
+						* evaluationFunctionValues[j];
+				Rn[i][j - i] = value;
 			}
 		}
 		return Rn;
 	}
-	
+
 	/**
 	 * Returns the expected lambda return for each time moment
-	 * @param Rn n-step return
-	 * @param Rt simple return 
-	 * @return 
+	 * 
+	 * @param Rn
+	 *            n-step return
+	 * @param Rt
+	 *            simple return
+	 * @return
 	 */
-	private double[] computeLabdaReturn(double[][] Rn, double[] Rt){
+	private double[] computeLabdaReturn(double[][] Rn, double[] Rt) {
 		double[] RtLambda = new double[Rt.length];
-		for(int i=0; i<RtLambda.length; i++){
-			double value=0.0;
-			for(int j=0; j<Rt.length-i; j++){
-				value+=Math.pow(lambdaEligibilityRate, j)*Rn[i][j];
+		for (int i = 0; i < RtLambda.length; i++) {
+			double value = 0.0;
+			for (int j = 0; j < Rt.length - i; j++) {
+				value += Math.pow(lambdaEligibilityRate, j) * Rn[i][j];
 			}
-			value+=Math.pow(lambdaEligibilityRate, Rt.length-i)*Rt[i];
-			RtLambda[i]=value;
+			value += Math.pow(lambdaEligibilityRate, Rt.length - i) * Rt[i];
+			RtLambda[i] = value;
 		}
 		return RtLambda;
 	}
-	
+
 	/**
 	 * Updating weights of evaluation function
 	 */
-	private void updateWeights(double[] RTLambda, double[][] evaluationFunctionGradient, double[] evluationFunctionValues)
-	{
-		for(int i=0; i<weights.length; i++){
-			double value=0.0;
-			for(int j=0; j<RTLambda.length; j++){
-				value+=(RTLambda[j]-evluationFunctionValues[j])*evaluationFunctionGradient[j][i];
+	private void updateWeights(double[] RTLambda,
+			double[][] evaluationFunctionGradient,
+			double[] evluationFunctionValues) {
+		for (int i = 0; i < weights.length; i++) {
+			double value = 0.0;
+			for (int j = 0; j < RTLambda.length; j++) {
+				value += (RTLambda[j] - evluationFunctionValues[j])
+						* evaluationFunctionGradient[j][i];
 			}
-			weights[i]=weights[i]+alfaLearningRate*value;
+			weights[i] = weights[i] + alfaLearningRate * value;
 		}
 	}
 }
