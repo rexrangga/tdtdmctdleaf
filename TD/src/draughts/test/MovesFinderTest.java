93bf1c1d8227422c673acdc70c7b6129c3f06130
mateusz.wenus <mateusz.wenus@edb23e68-2225-11de-9288-5f72e443383c>
2009-05-05 21:05:21 +0000

    moves finder zwraca rowniez nielegalne ruchy

---
+++ TD/src/draughts/test/MovesFinderTest.java
@@ -0,0 +1,104 @@
+package draughts.test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+
+import draughts.Author;
+import draughts.Checker;
+import draughts.MoveMessage;
+import draughts.MovesFinder;
+import draughts.Sort;
+
+public class MovesFinderTest {
+
+	private static final int BOARD_SIZE = 10;
+
+	private static Checker[][] createEmptyBoard() {
+		Checker[][] board = new Checker[BOARD_SIZE][];
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			Checker[] temp = new Checker[BOARD_SIZE];
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				temp[j] = new Checker(i, j);
+				if ((i + j) % 2 != 0) {
+					temp[j].setKind(Sort.blankBlack);
+				} else {
+					temp[j].setKind(Sort.blankWhite);
+				}
+			}
+			board[i] = temp;
+		}
+		return board;
+	}
+
+	private static void testEmpty() {
+		Checker[][] board = createEmptyBoard();
+		Set<?> s = new MovesFinder(board, Author.owner).getLegalMoves();
+		Assert.assertTrue(s.isEmpty());
+		System.out.println("+ success");
+	}
+
+	/**
+	 * A checker on empty board - one possible move
+	 */
+	private static void testSimple() {
+		Checker[][] board = createEmptyBoard();
+		board[9][0].setKind(Sort.fullWhite);
+		Set<List<MoveMessage>> s = new MovesFinder(board, Author.owner).getLegalMoves();
+		Assert.assertNotNull(s);
+		Assert.assertTrue(s.size() == 1);
+		List<MoveMessage> list = s.iterator().next();
+		Assert.assertNotNull(list);
+		Assert.assertTrue(list.size() == 1);
+		MoveMessage mm = list.get(0);
+		Assert.assertTrue(mm.getFirst().getI() == 9);
+		Assert.assertTrue(mm.getFirst().getJ() == 0);
+		Assert.assertTrue(mm.getSecond().getI() == 8);
+		Assert.assertTrue(mm.getSecond().getJ() == 1);
+		System.out.println("+ success");
+	}
+
+	private static void testSimple2() {
+		Checker[][] board = createEmptyBoard();
+		board[9][2].setKind(Sort.fullWhite);
+		Set<List<MoveMessage>> s = new MovesFinder(board, Author.owner).getLegalMoves();
+		Assert.assertNotNull(s);
+		Assert.assertTrue(s.size() == 2);
+		Iterator<List<MoveMessage>> it = s.iterator();
+		List<MoveMessage> list = it.next();
+		Assert.assertNotNull(list);
+		Assert.assertTrue(list.size() == 1);
+		list = it.next();
+		Assert.assertNotNull(list);
+		Assert.assertTrue(list.size() == 1);
+		System.out.println("+ success");
+	}
+
+	private static void testBeating() {
+		Checker[][] board = createEmptyBoard();
+		board[9][2].setKind(Sort.fullWhite);
+		board[8][3].setKind(Sort.fullBlack);
+		Set<List<MoveMessage>> s = new MovesFinder(board, Author.owner).getLegalMoves();
+		Assert.assertNotNull(s);
+		Assert.assertTrue(s.size() == 1);
+		Iterator<List<MoveMessage>> it = s.iterator();
+		List<MoveMessage> list = it.next();
+		Assert.assertNotNull(list);
+		Assert.assertTrue(list.size() == 1);
+		MoveMessage mm = list.get(0);
+		Assert.assertEquals(mm.getFirst().getI(), 9);
+		Assert.assertEquals(mm.getFirst().getJ(), 2);
+		Assert.assertEquals(mm.getSecond().getI(), 7);
+		Assert.assertEquals(mm.getSecond().getJ(), 4);
+		System.out.println("+ success");
+	}
+
+	public static void main(String[] args) {
+		testEmpty();
+		testSimple();
+		testSimple2();
+		testBeating();
+	}
+}
