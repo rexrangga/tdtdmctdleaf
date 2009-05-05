e089fbe7265c2bc13ddba1e201d738925d5649c9
podrazaa <podrazaa@edb23e68-2225-11de-9288-5f72e443383c>
2009-05-05 19:39:21 +0000

    Updated evaluation function calculations.

--- trunk/TD/src/draughts/TD.java
+++ trunk/TD/src/draughts/TD.java
@@ -4,33 +4,36 @@ package draughts;
  * Interface used by every TD implementation
  */
 public abstract class TD {
-	
+
 	protected double[] weights = null;
-	
-	public TD(double[] initialWeights){
-		weights=initialWeights;	
+	private double a = 0;
+	private double b = 0;
+
+	public TD(double[] initialWeights, double a, double b) {
+		weights = initialWeights;
+		this.a = a;
+		this.b = b;
 	}
-	
+
 	/*
-	 * Calculates value of the evaluation function for given evaluation function features;
+	 * Calculates value of the evaluation function for given evaluation function
+	 * features;
 	 */
-	public double calculateEvaluationFunction(double[] features){
-		if(features==null || features.length!=weights.length)
+	public double calculateEvaluationFunction(double[] features) {
+		if (features == null || features.length != weights.length)
 			return 0;
-		
+
 		double sum = 0.0;
-		
-		for(int i =0; i<weights.length; i++){
-			sum+=features[i]*weights[i];
+
+		for (int i = 0; i < weights.length; i++) {
+			sum += features[i] * weights[i];
 		}
-		
-		return sum;
+
+		return a * Math.tanh(b * sum);
 	}
-	
+
 	/*
 	 * Provides single game data, that should be used in the learning process.
 	 */
 	public abstract void updateWeights(GameData gameData);
 }
-
-
