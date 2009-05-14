97755355ee18427441fd08b0211652dd48497314
mateusz.wenus <mateusz.wenus@edb23e68-2225-11de-9288-5f72e443383c>
2009-05-14 19:03:04 +0000

    wypelnia sie game data

--- trunk/TD/src/draughts/GameData.java
+++ trunk/TD/src/draughts/GameData.java
@@ -14,15 +14,24 @@ public class GameData {
 	}
 
 	/**
-	 * Holds evaluation function features values for every time moment t =
-	 * 1,2.....T.
+	 * Holds evaluation function features values for every time moment t = 1,2.....T.
 	 */
-	public List<double[]> evaluationFunctionFeatures;
+	public List<double[]> evaluationFunctionFeatures = new ArrayList<double[]>();
 
 	/**
 	 * Holds board situations after each move.
 	 */
-	public List<CheckerModel[][]> m_board;
+	public List<CheckerModel[][]> m_board = new ArrayList<CheckerModel[][]>();
+
+	/**
+	 * Holds principal variation nodes for each move
+	 */
+	public List<CheckerModel[][]> principalVariation = new ArrayList<CheckerModel[][]>();
+
+	/**
+	 * Holds evaluations of board situations after each move
+	 */
+	public List<Double> statesEvaluations = new ArrayList<Double>();
 
 	/**
 	 * Holds information what kind of checkers was the algorithm using.
--- trunk/TD/src/draughts/moves/Mtd.java
+++ trunk/TD/src/draughts/moves/Mtd.java
@@ -5,9 +5,11 @@ import java.util.Set;
 
 import draughts.Checker;
 import draughts.CheckerModel;
+import draughts.GameData;
 import draughts.ITD;
 import draughts.MoveMessage;
 import draughts.MovesFinder;
+import draughts.Pair;
 import draughts.Player;
 
 public class Mtd {
@@ -16,10 +18,15 @@ public class Mtd {
 	private static final int MAX_DEPTH = 5;
 
 	private Checker[][] originalBoard;
+	private GameData gameData = new GameData();
 
 	private AlphaBetaWithMemory alphaBetaWithMemory = new AlphaBetaWithMemory();
 
 	public Mtd() {
+	}
+
+	public GameData getGameData() {
+		return gameData;
 	}
 
 	public List<MoveMessage> getBestMove(Checker[][] bboard, int f, int maxDepth, Player me, ITD td) {
@@ -29,14 +36,20 @@ public class Mtd {
 		MovesFinder mf = new MovesFinder(board, me.getMAuthor());
 		Set<List<MoveMessage>> legalMoves = mf.getLegalMoves();
 		List<MoveMessage> bestMove = null;
-		int bestValue = Integer.MIN_VALUE;
+		CheckerModel[][] bestVariation = null;
+		double bestValue = Double.NEGATIVE_INFINITY;
 		for (List<MoveMessage> move : legalMoves) {
-			int currentValue = evaluate(board, f, maxDepth, me, td);
+			Pair<Double, CheckerModel[][]> p = evaluate(board, f, maxDepth, me, td);
+			double currentValue = p.getFirst();
 			if (currentValue > bestValue) {
 				bestMove = move;
 				bestValue = currentValue;
+				bestVariation = p.getSecond();
 			}
 		}
+		gameData.m_board.add(BoardUtils.performMoves(board, bestMove));
+		gameData.statesEvaluations.add(bestValue);
+		gameData.principalVariation.add(bestVariation);
 		return bestMove;
 	}
 
@@ -44,25 +57,29 @@ public class Mtd {
 		return getBestMove(board, F, MAX_DEPTH, me, td);
 	}
 
-	private int evaluate(CheckerModel[][] board, int f, int maxDepth, Player me, ITD td) {
-		int g = f;
-		int upperBound = Integer.MAX_VALUE;
-		int lowerBound = Integer.MIN_VALUE;
+	private Pair<Double, CheckerModel[][]> evaluate(CheckerModel[][] board, double f, int maxDepth, Player me, ITD td) {
+		double g = f;
+		CheckerModel[][] principalVariation = null;
+		double upperBound = Double.POSITIVE_INFINITY;
+		double lowerBound = Double.NEGATIVE_INFINITY;
 		while (lowerBound < upperBound) {
-			int beta;
+			double beta;
 			if (g == lowerBound) {
 				beta = g + 1;
 			} else {
 				beta = g;
 			}
 			Node node = new Node(board);
-			g = alphaBetaWithMemory.evaluate(node, beta - 1, beta, maxDepth, true, me, td, originalBoard);
+			Pair<Double, CheckerModel[][]> p = alphaBetaWithMemory.evaluate(node, beta - 1, beta, maxDepth, true, me,
+					td, originalBoard);
+			g = p.getFirst();
+			principalVariation = p.getSecond();
 			if (g < beta) {
 				upperBound = g;
 			} else {
 				lowerBound = g;
 			}
 		}
-		return g;
+		return new Pair<Double, CheckerModel[][]>(g, principalVariation);
 	}
 }
