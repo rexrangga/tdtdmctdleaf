151a5c130c55f5f34d7c8291cfdcd1924809c480
mateusz.wenus <mateusz.wenus@edb23e68-2225-11de-9288-5f72e443383c>
2009-06-18 10:38:10 +0000

    ostatnie poprawki

--- TD/src/draughts/Main.java
+++ TD/src/draughts/Main.java
@@ -26,39 +26,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		/*
-		 * double[] weights = { 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,
-		 * 0.5, 0.5, 0.5 }; ArrayList<double[]> features = new
-		 * ArrayList<double[]>(); TDMC tdmc = new TDMC(weights, 0.5, 0.1, 0.1,
-		 * 0.5, 0.5); GameData gd = new GameData();
+		 * double[] weights = { 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5 }; ArrayList<double[]>
+		 * features = new ArrayList<double[]>(); TDMC tdmc = new TDMC(weights, 0.5, 0.1, 0.1, 0.5, 0.5); GameData gd =
+		 * new GameData();
 		 * 
-		 * double[] features1 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
-		 * double[] features2 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
-		 * double[] features3 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
-		 * double[] features4 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
-		 * double[] features5 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
+		 * double[] features1 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 }; double[] features2 = { 2, 2, 2, 3, 4, 2, 2, 2,
+		 * 1, -1, 0, 1 }; double[] features3 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 }; double[] features4 = { 2, 2, 2,
+		 * 3, 4, 2, 2, 2, 1, -1, 0, 1 }; double[] features5 = { 2, 2, 2, 3, 4, 2, 2, 2, 1, -1, 0, 1 };
 		 * 
-		 * gd.evaluationFunctionFeatures.add(features1);
-		 * gd.evaluationFunctionFeatures.add(features2);
-		 * gd.evaluationFunctionFeatures.add(features3);
-		 * gd.evaluationFunctionFeatures.add(features4);
+		 * gd.evaluationFunctionFeatures.add(features1); gd.evaluationFunctionFeatures.add(features2);
+		 * gd.evaluationFunctionFeatures.add(features3); gd.evaluationFunctionFeatures.add(features4);
 		 * gd.evaluationFunctionFeatures.add(features5);
 		 * 
-		 * // gd.m_board = null; gd.playerCheckersSort = Author.opponent;
-		 * gd.startingCheckersSort = Author.opponent;
+		 * // gd.m_board = null; gd.playerCheckersSort = Author.opponent; gd.startingCheckersSort = Author.opponent;
 		 * 
 		 * tdmc.updateWeights(gd);
 		 */
-		final String[] finalArgs=args;
+		final String[] finalArgs = args;
 		java.awt.EventQueue.invokeLater(new Runnable() {
 
 			public void run() {
 				Frame f = new Frame();
 				f.setVisible(true);
-				if(finalArgs.length>0){
+				if (finalArgs.length > 0 && finalArgs[0].trim().length() > 0) {
 					f.setArtificialGame(true);
-					
+
 					try {
-						f.setItd(PlayerUtils.loadPlayer(finalArgs[0],PlayerKind.NO_LEARNING));
+						f.setItd(PlayerUtils.loadPlayer(finalArgs[0], PlayerKind.NO_LEARNING));
 					} catch (IOException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
